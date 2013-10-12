fb2fcbc48c96c8ca84f5a294993412b4320208ea
shoota <shoota.dev@gmail.com>
2013-10-13 01:06:12 +0900

    add skeleton UT methods

--- test/ltsv2obj-spec.js
+++ test/ltsv2obj-spec.js
@@ -3,28 +3,51 @@
     assert = require('assert'),
     evEmitter = require('events').EventEmitter;
 
-describe('generate ltsv2obj as default', function(){
-    var testFile_base = 'test/test-base.ltsv';
-    var ltsv = new ltsv2obj(testFile_base, {});
+var types = ['string', 'number', 'function', 'object'];
 
+describe('about generate ltsv2obj as default', function(){
+    var testFile_base = 'test/test-base.ltsv';
+    var ltsv_default = new ltsv2obj(testFile_base, {});
 
     // type
     it('should inherits EventEmitter', function(){
-        assert.ok(ltsv instanceof  evEmitter);
+        assert.ok(ltsv_default instanceof  evEmitter);
     });
 
     // property
     it('should has default property', function(){
-        ltsv.config.should.have.property('cache', false);
-        ltsv.should.have.property('path', testFile_base);
-        should.exist(ltsv.readStore);
-        should.not.exist(ltsv.data);
+        ltsv_default.config.should.have.property('cache', false);
+        ltsv_default.should.have.property('path', testFile_base);
+        should.exist(ltsv_default.readStore);
+        should.not.exist(ltsv_default.data);
     });
 
-    // function
-//    it('should has methods', function(){
-//
-//    });
+    //function
+    it('should has methods', function(){
 
+        // constructor
+        types[2].should.equal(typeof ltsv_default.ltsv2obj);
 
+        // prototype methods
+        types[2].should.equal(typeof ltsv_default.begin);
+        types[2].should.equal(typeof ltsv_default.validateFile);
+        types[2].should.equal(typeof ltsv_default.parseLTSV);
+        types[2].should.equal(typeof ltsv_default.getCache);
+    });
+});
+
+describe('about generate ltsv2obj with options', function(){
+
+    it('should has custom property');
+    it('should have data property when set cache option');
+
+});
+
+describe('about behaviour parseLTSV method', function(){
+    it('should parse LTSV');
+});
+
+describe('fire events', function(){
+    it('should fire add event per parse');
+    it('should fire end event in finish parse');
 });
