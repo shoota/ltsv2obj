b324355f2980e3a97e3aaea47e1fb10ebf23cf2a
shoota <shoota.dev@gmail.com>
2013-10-13 00:28:47 +0900

    unit test about type, property

--- test/ltsv2obj-spec.js
+++ test/ltsv2obj-spec.js
@@ -1,4 +1,30 @@
-﻿describe('ltsv2obj-test', function(){
-  it('will be write after');
-})
+﻿var ltsv2obj = require('../index.js').ltsv2obj,
+    should = require('should'),
+    assert = require('assert'),
+    evEmitter = require('events').EventEmitter;
 
+describe('generate ltsv2obj as default', function(){
+    var testFile_base = 'test/test-base.ltsv';
+    var ltsv = new ltsv2obj(testFile_base, {});
+
+
+    // type
+    it('should inherits EventEmitter', function(){
+        assert.ok(ltsv instanceof  evEmitter);
+    });
+
+    // property
+    it('should has default property', function(){
+        ltsv.config.should.have.property('cache', false);
+        ltsv.should.have.property('path', testFile_base);
+        should.exist(ltsv.readStore);
+        should.not.exist(ltsv.data);
+    });
+
+    // function
+//    it('should has methods', function(){
+//
+//    });
+
+
+});
